b0e222c0a3a8cbff712b98e9f78ad4a6eb2f1160
Aaron Boyarsky <chefboyarsky@gmail.com>
2018-01-25 23:43:02 -0500

    Basic initial page

---
+++ resources/views/root.blade.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <title>cyberart.io</title>
+        <link href="https://fonts.googleapis.com/css?family=Maven+Pro" rel="stylesheet">
+        <style>
+            #container {
+                display: table;
+                height: 100%;
+                margin: 0 auto;
+                width: 100%;
+            }
+            .content {
+                background: #2e3436;
+                padding: 40px;
+                text-align: center;
+                display: table-cell;
+                vertical-align: middle;
+            }
+        </style>
+    </head>
+    <body style="background-color: #000000; height: 100vh; overflow:hidden">
+        <div id="container" style="color:#FFFFFF; text-align: center" >
+            <div class="content" style="font-family: 'Maven Pro', sans-serif;">
+
+                <h1>cyberart.io</h1>
+                <br/>
+                <h3>
+                    Hackathon #1: Sound-Responsive Art - Feb. 10
+                </h3>
+                <br/>
+                <a href="https://join.slack.com/t/dccyber/shared_invite/enQtMzAzNzcxOTgwMDk3LTZiZWMyNGM5Y2UwMmY3NWIwYmZkOGIzNWY5NTAzOWE5YzZlZjQ4NzRhYWM4NGRiY2RmNTZkZjc0MjFiZmU2ZTU">
+                    <img style="width: 200px; height: 50px;" src="/img/btn-sign-in-with-slack.svg">
+                </a>
+            </div>
+        </div>
+    </body>
+</html>
