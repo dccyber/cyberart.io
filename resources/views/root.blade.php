d760623f01580aa39fcb016a5642c0fa00a48ff0
Aaron Boyarsky <chefboyarsky@gmail.com>
2018-01-27 09:38:57 -0500

    Add Facebook widget

--- resources/views/root.blade.php
+++ resources/views/root.blade.php
@@ -20,18 +20,37 @@
         </style>
     </head>
     <body style="background-color: #000000; height: 100vh; overflow:hidden">
+        <!-- Load Facebook SDK for JavaScript -->
+        <div id="fb-root" ></div>
+        <script>(function(d, s, id) {
+                var js, fjs = d.getElementsByTagName(s)[0];
+                if (d.getElementById(id)) return;
+                js = d.createElement(s); js.id = id;
+                js.src = 'https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.11&appId=111591186193819&autoLogAppEvents=1';
+                fjs.parentNode.insertBefore(js, fjs);
+            }(document, 'script', 'facebook-jssdk'));</script>
+
         <div id="container" style="color:#FFFFFF; text-align: center" >
             <div class="content" style="font-family: 'Maven Pro', sans-serif;">
-
-                <h1>cyberart.io</h1>
-                <br/>
-                <h3>
-                    Hackathon #1: Sound-Responsive Art - Feb. 10
-                </h3>
                 <br/>
                 <a href="https://join.slack.com/t/cyberart-io/shared_invite/enQtMzAzNzcxOTgwMDk3LTZiZWMyNGM5Y2UwMmY3NWIwYmZkOGIzNWY5NTAzOWE5YzZlZjQ4NzRhYWM4NGRiY2RmNTZkZjc0MjFiZmU2ZTU">
                     <img style="width: 200px; height: 50px;" src="/img/btn-sign-in-with-slack.svg">
                 </a>
+                <br/><br/>
+                <div
+                    class="fb-page"
+                    data-href="https://www.facebook.com/cyberart.io/"
+                    data-tabs="timeline, events"
+                    data-small-header="true"
+                    data-adapt-container-width="true"
+                    data-hide-cover="false"
+                    data-show-facepile="true">
+                    <blockquote
+                        cite="https://www.facebook.com/cyberart.io/"
+                        class="fb-xfbml-parse-ignore">
+                        <a href="https://www.facebook.com/cyberart.io/">cyberart.io</a>
+                    </blockquote>
+                </div>
             </div>
         </div>
     </body>
